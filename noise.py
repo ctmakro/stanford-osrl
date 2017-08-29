1286ae9920b9ba6a630a2205ddc955bc993b4256
ctmakro <ctmakro@gmail.com>
2017-08-29 11:47:42 +0800

    new noise implementation, each randomstate per generator

--- noise.py
+++ noise.py
@@ -3,10 +3,11 @@ import numpy as np
 class one_fsq_noise(object):
     def __init__(self):
         self.buffer = np.array([0.])
+        self.state = np.random.RandomState()
 
     def one(self,size,noise_level=1.):
         # draw one gaussian
-        g = np.random.normal(loc=0.,scale=noise_level,size=size)
+        g = self.state.normal(loc=0.,scale=noise_level,size=size)
 
         if self.buffer.shape != size:
             self.buffer = np.zeros(size,dtype='float32')
